ccba6f19799bc9a0e7859670fa957f464fad7dbd
ivnogg <ivnocrypto@gmail.com>
2022-12-02 22:28:42 -0300

    fix(all): to run in new linux notebook

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,11 +5,11 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-// router.get('/', userController.index);
-// router.get('/:id', userController.show);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
 
-router.post('/', loginRequired, userController.store);
+router.post('/', userController.store);
 router.put('/', loginRequired, userController.update);
-router.delete('/', loginRequired, userController.delete);
+router.delete('/:id', loginRequired, userController.delete);
 
 export default router;
